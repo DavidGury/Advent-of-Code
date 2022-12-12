ebfd4c660b826e31f0888f969a5b1240accb933c
DG73 <davidgury@gmail.com>
2022-12-12 15:17:22 -0800

    renamed days 1-9, finished day 11

--- 2021/1/Day1-2021.py
+++ 2021/1/Day1-2021.py
@@ -3,7 +3,7 @@ def day01():
     inp = list(map(int,inp.read().splitlines()))
     pt1 = sum([1 for i in range(1,len(inp)) if inp[i] > inp[i-1]])
 
-    '''pt2 - moving groups of 3'''
+    '''pt2 - moving groups of 03'''
     pt2 = sum([1 for i in range(1,len(inp)) if sum(inp[i:i+3]) > sum(inp[i-1:i+2])])
 
     return pt1,pt2
--- 2022/11/day11_2022.py
+++ 2022/11/day11_2022.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass, field, InitVar
 from collections import deque
 import re
-from typing import Callable, Iterable
+import math
+from typing import Callable, Iterable, Optional
+
+
+OP_REF = {
+    '*': int.__mul__,
+    '+': int.__add__,
+    '^': int.__pow__
+}
 
 
 @dataclass(order=True)
@@ -19,20 +27,22 @@ class Monkey:
     def __post_init__(self, item_worry_lvls: list[int]):
         self.items.extend(item_worry_lvls)
 
-    def inspect_all(self) -> Iterable[tuple[int, int]]:
+    def inspect_all(self, worry_decrease_factor: int, lcm: Optional[int] = None) -> Iterable[tuple[int, int]]:
         # print(f'Monkey {self.index}:')
         while self.items:
-            yield self.inspect_next()
+            yield self.inspect_next(worry_decrease_factor, lcm)
 
-    def inspect_next(self) -> tuple[int, int]:
+    def inspect_next(self, worry_decrease_factor: int, lcm: Optional[int] = None) -> tuple[int, int]:
 
         self.n_inspections += 1
         item_wlvl = self.items.popleft()
         # print(f'\tMonkey inspects an item with a worry level of {item_wlvl}')
         w = self.op(item_wlvl, self.op_factor)
         # print(f'\t\tWorry level is {self.op} by {self.op_factor} to {w}')
-        w //= 3
-        # print(f'\t\tMonkey gets bored with item. Worry level is divided by 3 to {w}.')
+        w //= worry_decrease_factor
+        if lcm is not None:
+            w %= lcm
+        # print(f'\t\tMonkey gets bored with item. Worry level is divided by 03 to {w}.')
         if w % self.div_test:
             # print(f'\t\tCurrent worry level is not divisible by {self.div_test}.')
             # print(f'\t\tItem with worry level {w} is thrown to monkey {self.fail_monkey}.')
@@ -45,44 +55,58 @@ class Monkey:
         self.items.append(wlvl)
 
 
-op_ref = {
-    '*': int.__mul__,
-    '+': int.__add__,
-    '^': int.__pow__
-}
+@dataclass
+class Day11:
+    worry_decrease: int
+    n_rounds: int
+    input_path: InitVar[str]
+    lcm: int = 1
+    monkeys: list[Monkey] = field(default_factory=list)
 
-monkeys = []
-inp = open('input.txt').read().splitlines()
-i = 0
-while i < len(inp):
-    monkey_line, item_line, op_line, test_line, passtest_line, failtest_line = inp[i:i+6]
-    i += 7
-    oper, op_facstr = op_line.split(' ')[-2:]
-    if op_facstr == 'old' and oper == '+':
-        oper, op_facstr = '*', '2'
-    elif op_facstr == 'old' and oper == '*':
-        oper, op_facstr = '^', '2'
+    def __post_init__(self, input_path: str):
+        self.load_monkeys(open(input_path, 'r+').read().splitlines())
 
-    monkeys.append(
-        Monkey(
-            index=len(monkeys),
-            div_test=int(test_line.split(' ')[-1]),
-            op=op_ref[oper],
-            op_factor=int(op_facstr),
-            pass_monkey=int(passtest_line.split(' ')[-1]),
-            fail_monkey=int(failtest_line.split(' ')[-1]),
-            item_worry_lvls=[int(m) for m in re.findall(r'\d+', item_line)]
-        )
-    )
+    def load_monkeys(self, inp: list[str]):
+        i = 0
+        self.lcm = 1
+        while i < len(inp):
+            monkey_line, item_line, op_line, test_line, passtest_line, failtest_line = inp[i:i + 6]
+            i += 7
+            oper, op_facstr = op_line.split(' ')[-2:]
+            if op_facstr == 'old' and oper == '+':
+                oper, op_facstr = '*', '02'
+            elif op_facstr == 'old' and oper == '*':
+                oper, op_facstr = '^', '02'
 
-N_ROUNDS = 20
+            divisor = int(test_line.split(' ')[-1])
+            self.lcm *= (self.lcm * divisor) // math.gcd(self.lcm, divisor)
 
-for round_num in range(N_ROUNDS):
-    for i in range(len(monkeys)):
-        for rcv_monkey, wlvl in monkeys[i].inspect_all():
-            monkeys[rcv_monkey].receive_item(wlvl)
+            self.monkeys.append(
+                Monkey(
+                    index=len(self.monkeys),
+                    div_test=divisor,
+                    op=OP_REF[oper],
+                    op_factor=int(op_facstr),
+                    pass_monkey=int(passtest_line.split(' ')[-1]),
+                    fail_monkey=int(failtest_line.split(' ')[-1]),
+                    item_worry_lvls=[int(m) for m in re.findall(r'\d+', item_line)]
+                )
+            )
 
-# for m in monkeys:
-#     print(m.items)
+    def solve(self):
+        for round_num in range(self.n_rounds):
+            for i in range(len(self.monkeys)):
+                for rcv_monkey, wlvl in self.monkeys[i].inspect_all(self.worry_decrease, self.lcm):
+                    self.monkeys[rcv_monkey].receive_item(wlvl)
 
-print('Pt1:', int.__mul__(*sorted([m.n_inspections for m in monkeys])[-2:]))
+        return int.__mul__(*sorted([m.n_inspections for m in self.monkeys])[-2:])
+
+
+INP_PATH = 'input.txt'
+EX_path = 'ex1.txt'
+
+pt1 = Day11(worry_decrease=3, n_rounds=20, input_path=INP_PATH)
+pt2 = Day11(worry_decrease=1, n_rounds=10_000, input_path=INP_PATH)
+
+print(f'Pt1: {pt1.solve()}')
+print(f'Pt2: {pt2.solve()}')
